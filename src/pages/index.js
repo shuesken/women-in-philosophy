b59c27e8c986a48a950c050c40b16f2f9feb9c77
Simon <abeluga@posteo.net>
2022-07-13 17:07:20 +0100

    add downloadable document

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { graphql, StaticQuery, Link } from "gatsby";
+import { graphql, StaticQuery, Link, withPrefix } from "gatsby";
 import Img from "gatsby-image";
 import YouTube from "react-youtube";
 import Layout from "../components/layout";
@@ -41,7 +41,7 @@ const IndexPage = () => (
             A culture change is needed in philosophy. To foster this change we, the Demographics in Philosophy project, have done a few things. First, we have collected existing data on the representation of women in philosophy and put together a list of data based resources that may help us see more clearly what has gone wrong, how things have been changing over time, and to understand representation within different journals, professorial types, and ranks at different kinds of departments. On this site you can explore data on disparities between the proportion of those who are, and those who are not, under-represented in faculty hiring and publishing. You find resources for increasing diversity in philosophy and bibliographical information on surveys and large-scale empirical studies on the problem of underrepresentation in philosophy.
           </p>
           <p>
-            We have also held a series of listening sessions with journals and department heads on best practices and reviewed the literature to create a set of Good Practice Guidelines that departments, journals, and societies can <Link to="practices">sign up to</Link> – also available as a <Link to="TODOFIXME">downloadable document</Link>. The aim of this guide is to address factors that affect inclusion of underrepresented groups in philosophy - such as the methods of practicing philosophy, how it is taught, the content of philosophy and the workings of philosophy programs and their practices. The Good Practice Guidelines draw, and expand, on the APA’s Good Practices Guide and Blog on Diversity and Equity in Recruitment, the BPA/SWIP Good Practice Scheme, the Oxford University faculty of law Good Practices Guide, and the Canadian Society for Women in Philosophy Guidelines for Conference Hosting.
+            We have also held a series of listening sessions with journals and department heads on best practices and reviewed the literature to create a set of Good Practice Guidelines that departments, journals, and societies can <Link to="practices">sign up to</Link> – also available as a <a href={withPrefix("/GoodPractices.pdf")}>downloadable document</a>. The aim of this guide is to address factors that affect inclusion of underrepresented groups in philosophy - such as the methods of practicing philosophy, how it is taught, the content of philosophy and the workings of philosophy programs and their practices. The Good Practice Guidelines draw, and expand, on the APA’s Good Practices Guide and Blog on Diversity and Equity in Recruitment, the BPA/SWIP Good Practice Scheme, the Oxford University faculty of law Good Practices Guide, and the Canadian Society for Women in Philosophy Guidelines for Conference Hosting.
           </p>
 
 
