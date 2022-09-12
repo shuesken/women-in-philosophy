982644e8251f46a8abb51b45868bb90bbf3eaef1
Simon <abeluga@posteo.net>
2022-09-12 11:21:12 +0200

    add scientific advisors

--- src/pages/about.js
+++ src/pages/about.js
@@ -199,6 +199,189 @@ export default function About() {
       </Member>
       <br />
       <br />
+      <h1>Scientific Advisory Board</h1>
+      <Member
+        name="David Sobel"
+        photo={profileImage.sobel}
+        position=""
+        className="scientific"
+      >
+        David Sobel is Irwin and Marjorie Guttag Professor of Ethics and Political Philosophy in the Philosophy Dept at Syracuse University. He works primarily in ethics and political philosophy. Much of his research has focused on understanding what makes something valuable. His recent research has also explored how demanding our moral obligations can be, problems for some libertarian conceptions of our natural rights, as well as the best understanding of what makes something good for us. He is a founding co-editor of Oxford Studies in Political Philosophy and co-edits the blog PEA Soup. His From Valuing to Value was published by OUP in 2017.
+      </Member>
+      <Member
+        name="Becky Vartabedian"
+        photo={profileImage.vartabedian}
+        position=""
+        className="scientific"
+      >
+        Dr. Becky Vartabedian is Associate Professor of philosophy at Regis University in Denver, Colorado (USA). Her research is in contemporary Continental philosophy and its engagements with the history of philosophy, and in critical phenomenology, especially critical race phenomenology and whiteness studies. Dr. Vartabedian also serves as the Community, Culture, and Programs Strategist at Compass Ethics, an organizational ethics consultancy. Dr. Vartabedian is committed to inclusive teaching and research practices and she believes philosophy is at its best when it includes as many voices as possible.
+      </Member>
+      <Member
+        name="Henry Richardson"
+        photo={profileImage.richardson}
+        position=""
+        className="scientific"
+      >
+        Henry S. Richardson is Professor of Philosophy at Georgetown University.  He works on various aspects of what the Germans call “practical philosophy” and has published books on practical reasoning, democratic theory, medical research ethics, and our moral authority.  From 2008 – 2018, he was the editor-in-chief of Ethics, where he worked to assemble a more diverse team of associate editors and to encourage a broader range of articles, including articles on race and racism.  From 2010 – 2013, he was a member of the Commission on the Ethics of Scientific Knowledge and Technology (COMEST), a UNESCO body.  He has served the American Philosophical Association as a member of the advisory board for their Publication Ethics Project (2017 – 2019) and as a member of the Executive Committee of its Eastern Division (2018 – 2021)
+      </Member>
+      <Member
+        name="Alexander Guerrero"
+        photo={profileImage.guerrero}
+        position=""
+        className="scientific"
+      >
+        Alexander Guerrero, JD, PhD, is Professor of Philosophy at Rutgers University.  His research is primarily in political philosophy, moral philosophy, legal philosophy, and social epistemology.  He is the author of the forthcoming book, Lottocracy: A New Kind of Democracy (Oxford University Press), as well as the co-editor (with Elizabeth Harman) of the forthcoming Norton Introduction to Ethics.  He regularly teaches moral, legal, and political philosophy, as well as African, Latin American, and Native American Philosophy.  He is the Editor-in-Chief of Philosophy Compass, an Associate Editor at Ethics, and serves on the Advisory Board of Oxford Studies in Political Philosophy.
+      </Member>
+
+      <Member
+        name="Anthony Gillies"
+        photo={profileImage.gillies}
+        position=""
+        className="scientific"
+      >
+        Thony Gillies is the Sherwin Scott Professor of Philosophy at the University of Arizona, and is core faculty member in the Center for the Philosophy of Freedom and an affiliated faculty member in Linguistics. In 2014 he was the White Distinguished Professor in the Philosophy of Language at the University of Chicago. His research covers topics in formal semantics, philosophical logic, formal epistemology, and various parts of the decision sciences.
+      </Member>
+      <Member
+        name="Jonathan Schaffer"
+        photo={profileImage.schaffer}
+        position=""
+        className="scientific"
+      >
+        Professor Jonathan Schaffer earned his PhD in Philosophy from Rutgers University in 1999. He specializes in metaphysics, and has taught at the University of Houston, the University of Massachusetts-Amhert, and the Australian National University, before returning to Rutgers in 2011.
+      </Member>
+      <Member
+        name="Michael Otsuka"
+        photo={profileImage.otsuka}
+        position=""
+        className="scientific"
+      >
+        Michael Otsuka is a Professor of Philosophy at Rutgers University, New Brunswick. He is the author of Libertarianism without Inequality (OUP 2003) plus a number of articles in journals such as Philosophy & Public Affairs and Ethics, mainly on topics in normative ethics and distributive justice. His current research interests encompass the value and metaphysics of risks of harm and chances of benefits; reciprocity and cooperation; risk-pooling, pensions, and insurance; the nature of money and debt; prioritarianism, egalitarianism, and the separateness of persons. The focus of his teaching is on philosophy and public policy as well as moral and political philosophy. He posts blogs on Medium and threads on Twitter on issues related to public policy. Most of his posts have been on pensions, though he has also posted on public health (Covid), health insurance, higher education, and the measure of inflation.
+      </Member>
+      <Member
+        name="Abigail Stewart"
+        photo={profileImage.stewart}
+        position=""
+        className="scientific"
+      >
+        Abigail Stewart is Sandra Schwartz Tangri Distinguished University Professor of
+        Psychology and Gender and Women’s Studies at the University of Michigan. She has a
+        Ph.D. in Psychology and Social Relations from Harvard University, an M.Sc. in Social
+        Psychology from London School of Economics, and a B.A. from Wesleyan University.
+        Her research interests include political activism, personality development and change
+        in the context of experience and social history, and institutional change in higher
+        education. She is coauthor with Virginia Valian of An Inclusive Academy (MIT Press,
+        2018) and with Sarah Fenstermaker of Gender, considered: Feminist reflections across
+        the US social sciences (Palgrave/Macmillan, 2020).
+      </Member>
+      <Member
+        name="Eric Watkins"
+        photo={profileImage.watkins}
+        position=""
+        className="scientific"
+      >
+        Eric Watkins is a Distinguished Professor of Philosophy at the University of California, San Diego. He has written two monographs (Kant and the Metaphysics of Causality [2005] and Kant on Laws [2019], both with Cambridge University Press), and published dozens of articles in leading international journals. He served as the Director of Graduate Studies at UC-San Diego for five years, and has served on the Board of Directors of the Journal of the History of Philosophy.
+      </Member>
+      <Member
+        name="Christina Straehle"
+        photo={profileImage.straehle}
+        position=""
+        className="scientific"
+      >
+        <p>
+          Christine Straehle is Professor for Practical Philosophy at the University of Hamburg. Before joining the University of Hamburg, she was Professor of Ethics and Applied Ethics at the University of Ottawa, and Professor of Social Philosophy at UQAM. Since her arrival at the University of Ottawa, she has also been recruited as the inaugural and founding director of the Centre for Philosophy, Politics and Economics in the Faculty of Philosophy at Groningen University in 2016, where she also held the Chair in Philosophy and Public Affairs. <br />
+        </p><p>Straehle’s work is at the intersection of moral and political philosophy and public policy, which is to say that she examines the moral justifiability of existing public policies and makes normative recommendations on how to design just public policies. Straehle starts from the premise that addressing societal challenges such as labor shortages or the ethical implications of technologies of assisted reproduction benefit from philosophical analysis and reflection, and interdisciplinary collaboration. Results of this work are numerous articles, several edited collections, including The Political Philosophy of Refuge (Cambridge University Press) and Vulnerability, Autonomy and Applied Ethics (Routledge 2017). Her latest work is Debating the Surrogacy (Oxford University Press, forthcoming) and her monograph on Vulnerability, Territory, and Migration (in preparation). <br />
+        </p><p>Straehle has received prestigious fellowships and prizes in recognition of her work: she has been awarded a DAAD Professorship to spend the academic year 2015 - 2016 in Hamburg, has been a CEPPA fellow at the University of St.Andrews, at the Fondation Brocher in Switzerland, and Justitia Amplificata in Frankfurt, and is an Associate Member of Nuffield College, Oxford.In 2019, Straehle became a member of the College of the Royal Society of Canada, and was awarded the Kitty Newman Prize for Social and Political Philosophy.Since 2021, she has also been a member of the Hamburg Academy of Science.In 2022 - 2023, Straehle will be senior research fellow at the Swedish Collegium for Advanced Studies in Uppsala.
+        </p >
+      </Member >
+      <Member
+        name="Anthony Appiah"
+        photo={profileImage.appiah}
+        position=""
+        className="scientific"
+      >
+        Kwame Anthony Appiah is Professor of Philosophy and Law at NYU. [His father, Joe Appiah, was a Ghanaian lawyer and politician, and a member of the first post-independence Member of Parliament of Ghana, who went on to serve as Ghana’s Roving Ambassador and President of the Ghana Bar Association. His mother, Peggy Appiah, was an author and art scholar, and the daughter of Britain’s post-War Chancellor of the Exchequer, Sir Stafford Cripps.] Professor Appiah was born in London, where his parents met, but moved as an infant to Kumasi, Ghana, where he grew up, and where his three sisters were born. He took BA and PhD degrees in philosophy at Cambridge and has taught philosophy in Ghana, France, Britain, and the United States, with professorships at Yale, Cornell, Duke, Harvard, and Princeton. He explored questions of African and African-American identity in In My Father’s House: Africa in the Philosophy of Culture; examined the cultural dimensions of global citizenship in Cosmopolitanism: Ethics in a World of Strangers; investigated the social and individual importance of identity in The Ethics of Identity; among his other books is one about W.E.B. DuBois and another about the idea of honor. (He’s also written three mystery novels.) He has been President of the PEN American Center, the Modern Language Association, the Eastern Division of the American Philosophical Association, and the American Academy of Arts and Letters, and serves on the Board of the American Academy of Arts and Sciences, the New York Public Library and New York’s Public Theater. In 2012 he received the National Humanities Medal from President Obama. His most recent book is The Lies that Bind: Rethinking Identity.
+      </Member>
+      <Member
+        name="Craig Callender"
+        photo={profileImage.callender}
+        position=""
+        className="scientific"
+      >
+        <p>
+          I'm Professor of Philosophy and a founder and Director of  Faculty and the Institute for Practical Ethics
+          at the University of California, San Diego. I’m President-Elect of the Philosophy of Science Association (2023-) and sit on the Freedom and Responsibility in Science Committee of the International Science Council, Paris.
+        </p>
+        <p>
+          From 1996-2000 I worked in the Department of Philosophy, Logic & Scientific Method at the London School of Economics. I obtained my Ph.D. from Rutgers University in 1997.
+        </p>
+        <p>
+          My main area of research and teaching is philosophy of science. My book What Makes Time Special? (Oxford University Press, 2017) won the 2018 Lakatos Award and the 2022 Suppes Prize.
+        </p>
+      </Member>
+      <Member
+        name="Dan Korman"
+        photo={profileImage.korman}
+        position=""
+        className="scientific"
+      >
+        Dan Korman is a professor of philosophy at the University of California, Santa Barbara.
+      </Member>
+      <Member
+        name="Valerie Tiberius"
+        photo={profileImage.tiberius}
+        position=""
+        className="scientific"
+      >
+        Valerie Tiberius is the Paul W. Frenzel Chair in Liberal Arts and Professor of Philosophy at the University of Minnesota.  Her work explores the ways in which philosophy and psychology can both contribute to the study of well-being and virtue.  She is the author of The Reflective Life: Living Wisely With Our Limits (Oxford 2008), Moral Psychology: A Contemporary Introduction (Routledge 2015), Well-Being as Value Fulfillment:  How We Can Help Others to Live Well (Oxford, 2018), and What Do You Want out of Life?: A Philosophical Guide to Figuring Out What Matters (Princeton University Press, 2023). She has published numerous articles on the topics of practical reasoning, prudential virtues, well-being, and the relationship between positive psychology and ethics, and has received grants from the Templeton Foundation and the National Endowment for the Humanities.  She served as President of the Central Division of the American Philosophical Association from 2016-17.
+      </Member>
+      <Member
+        name="Alfred Mele"
+        photo={profileImage.mele}
+        position=""
+        className="scientific"
+      >
+        Alfred R. Mele is the William H. and Lucyle T. Werkmeister Professor of Philosophy at Florida State University. He is the author of thirteen books and over 250 articles and editor of seven books. He is past director of two multi-million-dollar, interdisciplinary projects: the Big Questions in Free Will project (2010-13) and the Philosophy and Science of Self-Control project (2014-17). His latest book is Free Will: An Opinionated Guide (Oxford University Press, 2022).
+      </Member>
+      <Member
+        name="Diana Tietjens Meyers"
+        photo={profileImage.meyers}
+        position=""
+        className="scientific"
+      >
+        Diana Tietjens Meyers is Professor Emerita of Philosophy at the University of
+        Connecticut, Storrs. She has held the Laurie Chair in Women’s and Gender
+        Studies at Rutgers University and the Ellacuría Chair of Social Ethics at Loyola
+        University, Chicago. She has published five monographs: Inalienable Rights: A
+        Defense (1985, Columbia University Press), Self, Society, and Personal Choice
+        (1989, Columbia University Press); Subjection and Subjectivity: Psychoanalytic
+        Feminism and Moral Philosophy (1994, Routledge), Gender in the Mirror:
+        Cultural Imagery and Women’s Agency (2002, OUP) and Victims’ Stories and the
+        Advancement of Human Rights (OUP 2016). Being Yourself: Essays on Identity,
+        Action, and Social Life collects some of her articles and chapters (2004, Rowman
+        &amp; Littlefield). She has edited seven collections and two special journal issues and
+        published many journal articles and chapters in books. Her most recent edited
+        collection is Poverty, Agency, and Human Rights (2014, OUP). She currently
+        works in four main areas of philosophy – philosophy of self and action, feminist
+        ethics and aesthetics, and human rights. Her website is
+        https://dianatietjensmeyers.wordpress.com/
+
+      </Member>
+      <Member
+        name="Virginia Valian"
+        photo={profileImage.valian}
+        position=""
+        className="scientific"
+      >
+        <p>
+          Virginia Valian is Distinguished Professor of Psychology at Hunter College – CUNY, and is a member of the doctoral faculties of Psychology, Linguistics, and Speech-Language-Hearing Sciences at the CUNY Graduate Center.  Within Psychology, she is a member of three training areas – Cognitive and Comparative Psychology, Developmental, and Basic and Applied Social Psychology.
+        </p><p>
+          Valian directs the Language Acquisition Research Center at Hunter College, which studies the acquisition of syntax in young children and the relation between bilingualism and cognition in adults.  She examines children's early multi-word combinations and how they develop via corpus analysis and experiments.
+        </p><p>
+          Valian is also co-founder and director of Hunter's Gender Equity Project.  She performs work on the reasons behind women's slow advancement in the professions and proposes remedies for individuals and institutions.  She has written on sexual harassment, recognition, and on how to improve women's progress in the work world in general and academia in particular.
+        </p>
+      </Member>
+      <br />
+      <br />
       <h1>Collaborators</h1>
       <Member
         name="Jevin West"
@@ -364,6 +547,6 @@ export default function About() {
       >
         Jean Krebs is an Associate at Garfunkel Wild, P.C., and Senior Fellow at Gitenstein Institute for Health Law & Policy.
       </Member>
-    </Layout>
+    </Layout >
   );
 }
