7eeaeb90a47fa69eadaac842985ba401ae8aa69d
Simon <abeluga@posteo.net>
2022-11-27 15:05:16 +0100

    remove password

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,12 +69,6 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
-    {
-      resolve: '@mkitio/gatsby-theme-password-protect',
-      options: {
-        password: 'wip' // delete or `undefined` to disable password protection
-      }
-    },
 
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
